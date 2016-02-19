7e7be099d0a39ecea89fadd911efe34c7f375932
Nathaniel <chaneyna@gmail.com>
2016-02-19 11:06:00 -0500

    Ignored *.mod and *.txt

--- test.f90
+++ test.f90
@@ -1,6 +1,20 @@
+module module_test
+
+integer :: var
+
+end module
+
+module module_test_2
+
+integer :: var2
+
+end module
+
 program test
 
+use module_test
+
 print*,'hello'
+print*,var,var2
 
 end program
-
